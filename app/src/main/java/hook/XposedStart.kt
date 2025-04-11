49c44ee71f652deffd5e76d10c52b96d48dedf4c
JeffChen001218 <jeffchen001218@gmail.com>
2025-04-11 18:28:23 +0800

    update

--- app/src/main/java/hook/XposedStart.kt
+++ app/src/main/java/hook/XposedStart.kt
@@ -1,6 +1,7 @@
 package hook
 
-import android.app.AndroidAppHelper
+import android.app.Application
+import android.content.Context
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -19,61 +20,56 @@ class XposedStart : IXposedHookLoadPackage {
         // 获取需要的class (如果所hook的函数参数需要)
         // val wifiInfoCLz = XposedHelpers.findClass("android.net.wifi.WifiInfo", loadPackageParam.classLoader)
 
-        XposedHelpers.findAndHookMethod(
-            "android.net.wifi.WifiInfo",
-            loadPackageParam.classLoader,
-            "getSSID",
-            // 函数参数class（可能0~多个，这里没有就不传）
-            object : XC_MethodHook() {
-                @Throws(Throwable::class)
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    super.beforeHookedMethod(param)
-                    // 读取参数
-                    // val arg0 = param.args[0]
-                }
+        // val context = AndroidAppHelper.currentApplication() // 可能取不到，跟时机有关系
 
-                @Throws(Throwable::class)
-                override fun afterHookedMethod(param: MethodHookParam?) {
-                    super.afterHookedMethod(param)
-                    // modify return value
-                    param?.result = getValue(
-                        "ssid",
-                        AndroidAppHelper.currentApplication()
-                    ).also {
-                        XposedBridge.log("ssid:${it}")
-                    }
-                }
-            })
         XposedHelpers.findAndHookMethod(
-            "android.net.wifi.WifiInfo",
-            loadPackageParam.classLoader,
-            "getBSSID",
-            object : XC_MethodHook() {
-                @Throws(Throwable::class)
-                override fun afterHookedMethod(param: MethodHookParam?) {
-                    super.afterHookedMethod(param)
-                    param?.result = getValue(
-                        "bssid",
-                        AndroidAppHelper.currentApplication()
-                    ).also {
-                        XposedBridge.log("bssid:${it}")
-                    }
-                }
-            })
-        XposedHelpers.findAndHookMethod(
-            "android.net.wifi.WifiInfo",
-            loadPackageParam.classLoader,
-            "getMacAddress",
-            object : XC_MethodHook() {
-                @Throws(Throwable::class)
-                override fun afterHookedMethod(param: MethodHookParam?) {
-                    super.afterHookedMethod(param)
-                    param?.result = getValue(
-                        "mac",
-                        AndroidAppHelper.currentApplication()
-                    ).also {
-                        XposedBridge.log("mac:${it}")
-                    }
+            "android.content.ContextWrapper", loadPackageParam.classLoader, "attachBaseContext",
+            Context::class.java, object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val context = (param.thisObject as? Application) ?: return
+
+                    XposedHelpers.findAndHookMethod(
+                        "android.net.wifi.WifiInfo",
+                        loadPackageParam.classLoader,
+                        "getSSID",
+                        // 函数参数class（可能0~多个，这里没有就不传）
+                        object : XC_MethodHook() {
+                            @Throws(Throwable::class)
+                            override fun beforeHookedMethod(param: MethodHookParam) {
+                                super.beforeHookedMethod(param)
+                                // 读取参数
+                                // val arg0 = param.args[0]
+                            }
+
+                            @Throws(Throwable::class)
+                            override fun afterHookedMethod(param: MethodHookParam?) {
+                                super.afterHookedMethod(param)
+                                // modify return value
+                                param?.result = getValue("ssid", context)
+                            }
+                        })
+                    XposedHelpers.findAndHookMethod(
+                        "android.net.wifi.WifiInfo",
+                        loadPackageParam.classLoader,
+                        "getBSSID",
+                        object : XC_MethodHook() {
+                            @Throws(Throwable::class)
+                            override fun afterHookedMethod(param: MethodHookParam?) {
+                                super.afterHookedMethod(param)
+                                param?.result = getValue("bssid", context)
+                            }
+                        })
+                    XposedHelpers.findAndHookMethod(
+                        "android.net.wifi.WifiInfo",
+                        loadPackageParam.classLoader,
+                        "getMacAddress",
+                        object : XC_MethodHook() {
+                            @Throws(Throwable::class)
+                            override fun afterHookedMethod(param: MethodHookParam?) {
+                                super.afterHookedMethod(param)
+                                param?.result = getValue("mac", context)
+                            }
+                        })
                 }
             })
     }
